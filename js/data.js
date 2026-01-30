04aafda372c01febb249733fd9c2891893aed763
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-30 20:59:47 +0300

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,31 +1,21 @@
-// ====================== DATA ======================
+// VERİLER
 
-// Kullanıcı
 let user = {
   name: "Misafir",
   id: Math.floor(Math.random() * 9000 + 1000)
 };
 
-// Arkadaşlar
 let friends = [
   { name: "Ahmet", id: "1001", online: true },
   { name: "Ayşe", id: "1002", online: false }
 ];
 
-// Aktif sohbetler
 let activeChats = [];
 
-// Kanallar
 let channels = {
-  "Genel#0000": {
-    messages: [],
-    owner: null
-  }
+  "Genel#0000": { messages: [], owner: null }
 };
 
-// Okunmamış mesajlar
 let unreadMessages = {};
 
-// Aktif kanal
 let currentChannel = "Genel#0000";
-
