bd9d25f7b57f9cd754a48d45ab8b6fbc2fb863be
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-30 20:44:39 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,11 @@
-// main.js
-
-// HTML elemanları
 const messageInput = document.getElementById("messageInput");
 const sendBtn = document.getElementById("sendBtn");
 
-// Mesaj gönderme
-sendBtn.addEventListener("click", sendMessage);
-messageInput.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") sendMessage();
-});
+sendBtn.onclick = () => {
+    const text = messageInput.value;
+    if (text === "") return;
 
-function sendMessage() {
-    const text = messageInput.value.trim();
-    if (!text) return;
-
-    // ui.js içindeki fonksiyon
     addMessage("Sen", text);
-
     messageInput.value = "";
-}
+};
 
