c90de1b596bec4a20cc16698fb2f5272379af2ea
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-31 00:21:14 +0300

    Update chat.js

--- js/chat.js
+++ js/chat.js
@@ -1,1 +1,21 @@
+let currentUser = null;
 
+// Mesaj gönder
+function sendMessage(text){
+  if(!currentUser || !text) return;
+
+  db.ref("messages").push({
+    fromUid: currentUser.uid,
+    fromName: document.getElementById("userName").innerText,
+    text: text,
+    timestamp: Date.now()
+  });
+}
+
+// Mesajları dinle
+db.ref("messages").on("child_added", snapshot => {
+  const msg = snapshot.val();
+  const li = document.createElement("li");
+  li.textContent = `${msg.fromName}: ${msg.text}`;
+  document.getElementById("messagesList").appendChild(li);
+});
