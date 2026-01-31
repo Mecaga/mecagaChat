c8bb00ea4152671159241592735797c971c41bfb
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-31 21:25:54 +0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -28,3 +28,29 @@ function hideAllScreens() {
 function isMobile() {
   return window.innerWidth <= 768;
 }
+
+// ================= ARKADASLIK ISTEGI GONDERME =================
+function sendFriendRequest() {
+  const input = document.getElementById("addFriendInput").value.trim();
+  if (!input.includes("#")) return alert("Kullanıcı adı hatalı");
+
+  const [name, tag] = input.split("#");
+  const db = firebase.database();
+  const myUid = firebase.auth().currentUser.uid;
+
+  db.ref("users").once("value", snap => {
+    let targetUid = null;
+
+    snap.forEach(user => {
+      if (user.val().username === `${name}#${tag}`) {
+        targetUid = user.key;
+      }
+    });
+
+    if (!targetUid) return alert("Kullanıcı bulunamadı");
+    if (targetUid === myUid) return alert("Kendini ekleyemezsin");
+
+    db.ref(`friendRequests/${targetUid}/${myUid}`).set(true);
+    alert("Arkadaş isteği gönderildi");
+  });
+}
