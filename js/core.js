74e7248437b4a8ce6cdaba47696e8de1b3f1e9e9
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-31 18:24:52 +0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -152,3 +152,36 @@ window.deleteAccount = () => {
     location.reload();
   }).catch(err=>alert("❌ Hata: "+err.message));
 };
+window.showCreateChannel = () => {
+  channelForm.classList.remove("hidden");
+  joinForm.classList.add("hidden");
+};
+
+window.showJoinChannel = () => {
+  joinForm.classList.remove("hidden");
+  channelForm.classList.add("hidden");
+};
+
+window.createChannel = () => {
+  const name = channelNameInput.value;
+  if(!name) return alert("Kanal adı gerekli!");
+  const uid = auth.currentUser.uid;
+  const id = Math.floor(Math.random()*9000+1000); // basit ID
+
+  db.ref("channels/"+name+"#"+id).set({
+    creator: uid,
+    name: name
+  }).then(()=>alert("✅ Kanal oluşturuldu!"));
+};
+
+window.joinChannel = () => {
+  const fullName = joinChannelInput.value;
+  db.ref("channels/"+fullName).once("value").then(snap=>{
+    if(!snap.exists()) return alert("❌ Kanal bulunamadı!");
+    alert("✅ Kanal katıldınız!");
+  });
+};
+
+window.openGeneralChannel = () => {
+  alert("Genel Sohbet açıldı!");
+};
