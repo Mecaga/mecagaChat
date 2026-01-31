f300ee6c59e22c684bec08e2204de35d85a3c75d
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-31 16:39:17 +0300

    Update core.js

--- js/core.js
+++ js/core.js
@@ -1,12 +1,27 @@
+function showRegister() {
+  document.getElementById("loginScreen").classList.add("hidden");
+  document.getElementById("registerScreen").classList.remove("hidden");
+}
+
+function showLogin() {
+  document.getElementById("registerScreen").classList.add("hidden");
+  document.getElementById("loginScreen").classList.remove("hidden");
+}
+
 function login() {
   enterApp();
 }
 
 function register() {
+  let username = document.getElementById("regUsername").value;
+  if (username) {
+    document.getElementById("myUser").innerText = username + "#0001";
+  }
   enterApp();
 }
 
 function enterApp() {
-  document.getElementById("authScreen").classList.add("hidden");
+  document.getElementById("loginScreen").classList.add("hidden");
+  document.getElementById("registerScreen").classList.add("hidden");
   document.getElementById("mainScreen").classList.remove("hidden");
 }
