289d127635398b58f2c5078fc7fcad8abccfaedd
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-31 17:07:26 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,12 +3,12 @@ const firebaseConfig = {
   authDomain: "mecagachat-ec3a7.firebaseapp.com",
   databaseURL: "https://mecagachat-ec3a7-default-rtdb.firebaseio.com",
   projectId: "mecagachat-ec3a7",
-  storageBucket: "mecagachat-ec3a7.firebasestorage.app",
+  storageBucket: "mecagachat-ec3a7.appspot.com",
   messagingSenderId: "883254383531",
   appId: "1:883254383531:web:5622ae539f774bba64b8ee"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const auth = firebase.auth();
-const db = firebase.database();
+window.auth = firebase.auth();
+window.db = firebase.database();
