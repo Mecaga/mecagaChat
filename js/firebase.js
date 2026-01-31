40ca44094a0f9d26e8222848399bad632c08edbd
MECAGA <146643630+Mecaga@users.noreply.github.com>
2026-01-31 17:00:32 +0300

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,27 +1,14 @@
-// 🔹 Firebase Konfigürasyonu
 const firebaseConfig = {
   apiKey: "AIzaSyC46gx7XPMN607cZR0xPoryIXlWf7D3nuI",
   authDomain: "mecagachat-ec3a7.firebaseapp.com",
+  databaseURL: "https://mecagachat-ec3a7-default-rtdb.firebaseio.com",
   projectId: "mecagachat-ec3a7",
   storageBucket: "mecagachat-ec3a7.firebasestorage.app",
   messagingSenderId: "883254383531",
   appId: "1:883254383531:web:5622ae539f774bba64b8ee"
 };
 
-// Firebase başlat
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 const db = firebase.database();
-const storage = firebase.storage();
-
-// Kullanıcı durumu kontrolü
-auth.onAuthStateChanged(user => {
-  if(user){
-    currentUser = user;
-    console.log("Giriş yaptı:", user.uid);
-  } else {
-    currentUser = null;
-    console.log("Çıkış yapıldı");
-  }
-});
